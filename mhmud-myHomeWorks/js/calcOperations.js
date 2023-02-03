f0b425be74699c23916935d5af879013ba555218
mhmudak <mahmouud.akoum@gmail.com>
2023-02-03 10:38:59 +0200

    Added some css to the simple Calculator

---
+++ mhmud-myHomeWorks/js/calcOperations.js
@@ -0,0 +1,18 @@
+const addition = (a,b) => alert(a+b);
+
+const subtraction = (a,b) => alert(a-b);
+
+const multiplication = (a,b) => alert(a*b);
+
+const division = (a,b) => alert(a/b);
+// const a = 6;
+
+
+// const addition = (a,b) => {
+//     return a+b;
+// }
+
+// // console.log(addition(3,33));
+
+
+
